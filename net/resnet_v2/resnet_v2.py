88848b087f6f8a15a4d714cb55067e0398c5952f
MachineLP <MachineLP@163.com>
2017-12-26 11:25:20 +0800

    Update resnet_v2.py

--- net/resnet_v2/resnet_v2.py
+++ net/resnet_v2/resnet_v2.py
@@ -1,51 +1,11 @@
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Contains definitions for the preactivation form of Residual Networks.
-
-Residual networks (ResNets) were originally proposed in:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Deep Residual Learning for Image Recognition. arXiv:1512.03385
-
-The full preactivation 'v2' ResNet variant implemented in this module was
-introduced by:
-[2] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Identity Mappings in Deep Residual Networks. arXiv: 1603.05027
-
-The key difference of the full preactivation 'v2' variant compared to the
-'v1' variant in [1] is the use of batch normalization before every weight layer.
-
-Typical use:
-
-   from tensorflow.contrib.slim.nets import resnet_v2
-
-ResNet-101 for image classification into 1000 classes:
-
-   # inputs has shape [batch, 224, 224, 3]
-   with slim.arg_scope(resnet_v2.resnet_arg_scope()):
-      net, end_points = resnet_v2.resnet_v2_101(inputs, 1000, is_training=False)
-
-ResNet-101 for semantic segmentation into 21 classes:
-
-   # inputs has shape [batch, 513, 513, 3]
-   with slim.arg_scope(resnet_v2.resnet_arg_scope(is_training)):
-      net, end_points = resnet_v2.resnet_v2_101(inputs,
-                                                21,
-                                                is_training=False,
-                                                global_pool=False,
-                                                output_stride=16)
+# -*- coding: utf-8 -*-
 """
+Created on 2017 10.17
+@author: liupeng
+wechat: lp9628
+blog: http://blog.csdn.net/u014365862/article/details/78422372
+"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
