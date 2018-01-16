ee7c4deafec31f85abcdc682de7bceb8edba2d48
MachineLP <MachineLP@163.com>
2018-01-16 15:09:44 +0800

    Update load_image.py

--- load_image/load_image.py
+++ load_image/load_image.py
@@ -59,21 +59,19 @@ def load_database_path(imgDir):
     return train_imgs, train_labels
 
 
-
-
 def get_next_batch_from_path(image_path, image_labels, pointer, IMAGE_HEIGHT=299, IMAGE_WIDTH=299, batch_size=64, is_train=True):
     batch_x = np.zeros([batch_size, IMAGE_HEIGHT,IMAGE_WIDTH,3])
     num_classes = len(image_labels[0])
     batch_y = np.zeros([batch_size, num_classes]) 
     for i in range(batch_size):  
         image = cv2.imread(image_path[i+pointer*batch_size])
-        image = cv2.resize(image, ((int(IMAGE_HEIGHT*1.5), int(IMAGE_WIDTH*1.5)))  
+        image = cv2.resize(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
         if is_train:
             image = random_flip(image)
             image = random_rotation(image)
             image = random_crop(image)
             image = random_exposure(image)
-        image = cv2.resize(image, (IMAGE_HEIGHT, IMAGE_WIDTH))  
+        # image = cv2.resize(image, (IMAGE_HEIGHT, IMAGE_WIDTH))  
         # 选择自己预处理方式：
         '''
         m = image.mean()
